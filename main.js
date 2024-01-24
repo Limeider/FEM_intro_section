8e63b6ff6411851e82b34c4fb3384ef81a648226
Limeider <139917489+Limeider@users.noreply.github.com>
2024-01-24 15:48:18 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+// Utility Functions to Get Elements
+const select = (selector) => document.querySelector(selector);
+const selectAll = (selector) => document.querySelectorAll(selector);
+
+// Get Elements from HTML
+const featuresAnchor = select("#features")
+const featuresDownArrow = select("#features-down-arrow")
+const featuresUpArrow = select("#features-up-arrow")
+const companyAnchor = select("#company")
+const companyDownArrow = select("#company-down-arrow")
+const companyUpArrow = select("#company-up-arrow")
+const menuOpen = select("#menu-open")
+const menuClose = select("#menu-close")
+const backDrop = select(".backdrop")
+const navItemsContainer = select(".nav-items-container")
+const featuresSubMenu = select(".features-sub-menu")
+const companySubMenu = select(".company-sub-menu")
+
+// Event Listeners
+featuresAnchor.addEventListener("mouseover", () => toggleArrows(featuresDownArrow, featuresUpArrow))
+featuresAnchor.addEventListener("mouseout", () => toggleArrows(featuresDownArrow, featuresUpArrow))
+companyAnchor.addEventListener("mouseover", () => toggleArrows(companyDownArrow, companyUpArrow))
+companyAnchor.addEventListener("mouseout", () => toggleArrows(companyDownArrow, companyUpArrow))
+
+menuOpen.addEventListener("click", function () {
+    navItemsContainer.classList.add("show")
+    menuOpen.style.display = "none"
+    menuClose.classList.add("show")
+    backDrop.classList.add("show")
+})
+
+menuClose.addEventListener("click", function () {
+    backDrop.classList.remove("show")
+    menuClose.classList.remove("show")
+    navItemsContainer.classList.remove("show")
+    menuOpen.style.display = "block"
+})
+
+featuresAnchor.addEventListener("click", () => toggleIfMenuOpen(featuresSubMenu))
+companyAnchor.addEventListener("click", () => toggleIfMenuOpen(companySubMenu))
+
+// Functions
+function toggleArrows(downArrow, upArrow) {
+    downArrow.classList.toggle("show")
+    upArrow.classList.toggle("show")
+}
+
+function toggleIfMenuOpen(subMenu) {
+    if (menuClose.classList.contains("show")) {
+        subMenu.classList.toggle("show")
+    }
+}
